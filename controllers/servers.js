33bb374c9ca3001a0ef385b9c75d47856da5d1b6
Georgikson <falyan2000@mail.ru>
2021-05-13 12:42:46 +0400

    added tab 'authors'

--- controllers/servers.js
+++ controllers/servers.js
@@ -22,26 +22,79 @@ const getAllData = (id) => {
     return finalData
 }
 
+const getAuthorsPublicList = authors => {
+    let authorsPublications = []
+
+    let authorsNames = authors.map(author => author['author'])
+    authorsNames = new Set(authorsNames)
+    
+    authorsNames.forEach(authorName => {
+        let publications = []
+        
+        for (let author of authors) {
+            if (author['author'] === authorName) {
+                publications.push(author)
+            }
+        }
+
+        authorsPublications.push(publications)
+    })
+
+    return authorsPublications
+}
+
+const getAuthorRating = (author, articles) => {
+    let authorRating = 0;
+
+    for (let article of author) {
+        let articleItem = articles.find(item => item['id'] === article['article_id'])
+        authorRating += +articleItem['rating']
+    }
+
+    return Math.round(authorRating * 100) / 100
+}
+
+const getAuthorsList = () => {
+    let authorsList = getAuthorsPublicList(authors)
+    let authorsData = []
+    let authorId = 0;
+    for (let author of authorsList) {
+        let authorRating = getAuthorRating(author, articles)
+
+        let authorData = {
+            id: authorId,
+            author: author[0]['author'],
+            publicCount: author.length,
+            rating: authorRating
+        }
+
+        authorsData.push(authorData)
+        authorId++
+    }
+
+    return authorsData
+}
+
+export const getPublications = (req, res) => {
+    if (req.params.name !== 'publications') {
+        res.sendFile(__dirname + '/404.html')
+    }
+
+    res.render('publications', {title: 'Publications', active: 'publications', data: articles})
+} 
+
 export const getArticle = (req, res) => {
     if (req.params.name !== 'article/id=:id') {
         res.sendFile(__dirname + '/404.html')
     }    
 
     let article = getAllData(req.params.id)
-
+    console.log(article)
     res.render('article', {title: 'Article', active: 'article', article: article['article'], authors: article['authors'], journal: article['journal']})
 }
 
-export const getPublications = (req, res) => {
-    if (req.params.name !== 'publications') {
-        res.sendFile(__dirname + '/404.html')
-    }    
+export const getAuthors = (req, res) => {
 
-    let dataConfig = {
-        minPage: 1,
-        articlesCount: 50,
-        pagesCount: Math.ceil(articles.length / 50),
-    }
-
-    res.render('publications', {title: 'Publications', active: 'publications', data: articles, config: dataConfig})
+    let autrhorsList = getAuthorsList(authors, articles)
+    res.render('authors', {title: 'Authors', active: 'authors', data: autrhorsList})
 }
--- routes/servers.js
+++ routes/servers.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import {getArticle, getPublications} from '../controllers/servers.js'
+import {getArticle, getPublications, getAuthors} from '../controllers/servers.js'
 const router = Router();
 
 router.get('/article/id=:id', getArticle)
 router.get('/publications', getPublications)
+router.get('/authors', getAuthors)
 
 export default router
